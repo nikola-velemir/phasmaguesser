b15603ee4720459be4b645d79ab47e1c6111d3d3
nikola-velemir <vnexa021@gmail.com>
2026-05-18 17:05:26 +0200

    [feat] template ghost elimination

--- back/service/src/main/java/com/ftn/sbnz/service/ghosts/service/IndentificatorService.java
+++ back/service/src/main/java/com/ftn/sbnz/service/ghosts/service/IndentificatorService.java
@@ -10,7 +10,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.drools.core.ObjectFilter;
 import org.kie.api.runtime.KieSession;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
