d3a50e49a8acafae151ba4070bba13ae360ce799
Nikola Velemir <nikola-velemir@localhost.localdomain>
2026-05-19 23:18:43 +0200

    [fix] layout

--- back/service/src/main/java/com/ftn/sbnz/service/config/KieConfig.java
+++ back/service/src/main/java/com/ftn/sbnz/service/config/KieConfig.java
@@ -14,8 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-
-
 @Configuration
 public class KieConfig {
     @Bean
@@ -29,40 +27,40 @@ public class KieConfig {
     }
 
     @Bean(name = "templateGhostSession")
-@Scope("prototype")
-public KieSession templateGhostSession() {
-    try {
-        System.setProperty("drools.dialect.java.compiler.lnglevel", "11");
-        
-        // 1. Kompajliranje šablona
-        InputStream template = new ClassPathResource("rules/ghosts.drt").getInputStream();
-        InputStream data = new ClassPathResource("rules/ghost-identities.xls").getInputStream();
-        ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
-        String templateDrl = converter.compile(data, template, 2, 1);
-        
-        KieServices ks = KieServices.Factory.get();
-        KieFileSystem kfs = ks.newKieFileSystem();
-        
-        // Upisujemo kompajlirani šablon (Nivo 0 - Filter)
-        kfs.write("src/main/resources/rules/ghosts_compiled.drl", templateDrl);
-        
-        // 2. DODAVANJE OSTALIH NIVOA U ISTI FAJLSISTEM
-        kfs.write(new ClassPathResource("rules/level1-traits.drl"));
-        kfs.write(new ClassPathResource("rules/level2-scoring.drl"));
-        kfs.write(new ClassPathResource("rules/level3-profiles.drl"));
-        
-        KieBuilder kieBuilder = ks.newKieBuilder(kfs);
-        kieBuilder.buildAll(); 
-        
-        if (kieBuilder.getResults().hasMessages(org.kie.api.builder.Message.Level.ERROR)) {
-            throw new RuntimeException("Greška u sintaksi pravila: " + kieBuilder.getResults().toString());
+    @Scope("prototype")
+    public KieSession templateGhostSession() {
+        try {
+            System.setProperty("drools.dialect.java.compiler.lnglevel", "11");
+
+            // 1. Kompajliranje šablona
+            InputStream template = new ClassPathResource("rules/ghosts.drt").getInputStream();
+            InputStream data = new ClassPathResource("rules/ghost-identities.xls").getInputStream();
+            ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
+            String templateDrl = converter.compile(data, template, 2, 1);
+
+            KieServices ks = KieServices.Factory.get();
+            KieFileSystem kfs = ks.newKieFileSystem();
+
+            // Upisujemo kompajlirani šablon (Nivo 0 - Filter)
+            kfs.write("src/main/resources/rules/ghosts_compiled.drl", templateDrl);
+
+            // 2. DODAVANJE OSTALIH NIVOA U ISTI FAJLSISTEM
+            kfs.write(new ClassPathResource("rules/level1-traits.drl"));
+            kfs.write(new ClassPathResource("rules/level2-scoring.drl"));
+            kfs.write(new ClassPathResource("rules/level3-profiles.drl"));
+
+            KieBuilder kieBuilder = ks.newKieBuilder(kfs);
+            kieBuilder.buildAll();
+
+            if (kieBuilder.getResults().hasMessages(org.kie.api.builder.Message.Level.ERROR)) {
+                throw new RuntimeException("Greška u sintaksi pravila: " + kieBuilder.getResults().toString());
+            }
+
+            KieModule kieModule = kieBuilder.getKieModule();
+            return ks.newKieContainer(kieModule.getReleaseId()).newKieSession();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Greška prilikom inicijalizacije Drools sesije: ", e);
         }
-        
-        KieModule kieModule = kieBuilder.getKieModule();
-        return ks.newKieContainer(kieModule.getReleaseId()).newKieSession();
-        
-    } catch (Exception e) {
-        throw new RuntimeException("Greška prilikom inicijalizacije Drools sesije: ", e);
     }
-}
 }
