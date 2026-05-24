6df496657092648532430030eeda5c4efbc33b9d
nikola-velemir <vnexa021@gmail.com>
2026-05-25 00:19:39 +0200

    [fix] tailwind messup on front app

---
+++ front/.angular/cache/21.2.11/phasmaguesser/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,44 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-BZFEAFSR.js";
+import "./chunk-WC4PITUE.js";
+import "./chunk-YEV2XGH7.js";
+import "./chunk-Q6L2SI4Q.js";
+import "./chunk-YK32BLOY.js";
+import "./chunk-3WH3RY3C.js";
+import "./chunk-RDXEEWVQ.js";
+import "./chunk-OOXH2RWB.js";
+import "./chunk-O5J3CNTX.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
